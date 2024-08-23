0e5e158c65d4e9ebdfb7490ec6ca3c16aa33cf71
MrDiallo13Oumar <do25595@gmail.com>
2024-08-23 16:29:57 +0000

    modif 16h30

--- src/app/pages/_ajustementStock/_component/add-ajustement/add-ajustement.component.ts
+++ src/app/pages/_ajustementStock/_component/add-ajustement/add-ajustement.component.ts
@@ -1,7 +1,10 @@
 import { Component, Inject, OnInit, Optional } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { AddAjustementStockComponent } from '../../_modal/add-ajustement-stock/add-ajustement-stock.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { AjustementStockService } from '../../_service/ajustement-stock.service';
 
 @Component({
   selector: 'app-add-ajustement',
@@ -9,27 +12,116 @@ import { AddAjustementStockComponent } from '../../_modal/add-ajustement-stock/a
   styleUrls: ['./add-ajustement.component.scss']
 })
 export class AddAjustementComponent implements OnInit {
-
+title = 'Formulaire Approvisionnement '
  // MISE A JOUR FIxiNG
  AjustementStock = new FormGroup({
   status: new FormControl(''),
-  produit: new FormControl('',Validators.required),
-  entrepot: new FormControl(''),
-  fournisseur: new FormControl('', Validators.required),
-  montantTotal: new FormControl(''),
+  id_produit: new FormControl('',Validators.required),
+  id_entrepot: new FormControl(''),
+  id_fournissseur: new FormControl('', Validators.required),
+  montant_total: new FormControl(''),
   quantite:new FormControl(''),
-   prixUniteAchat	:new FormControl(''),
- prixUniteVente	:new FormControl(''),
- datePeremption:new FormControl(''),
+  prix_unite_achat	:new FormControl(''),
+ prix_unite_vente	:new FormControl(''),
+ date_peremption:new FormControl(''),
 })
 
 
-constructor(
+constructor (
+  private service: AjustementStockService,
+  private snackBar: MatSnackBar,
+  private fb: FormBuilder
+) {}
+
+ngOnInit (): void {}
+
  
-) { }
+// imagePreview: string | ArrayBuffer | null = null
+// onFileChange (event: any) {
+//   const file = event.target.files[0]
+//   if (file) {
+//     const reader = new FileReader()
+//     reader.onload = (e: any) => {
+    
+//     }
+//     reader.readAsDataURL(file)
+//   }
+// }
+Produit = [
+  {
+    id: 1,
+    name: 'Fanta'
+  },
+  {
+    id: 2,
+    name: 'Ordinateur'
+  },
+  {
+    id: 3,
+    name: 'Chemise'
+  },
+  {
+    id: 4,
+    name: 'Tv'
+  }
+] 
+Fournisseur = [
+  {
+    id: 1,
+    name: 'Mahmud'
+  },
+  {
+    id: 2,
+    name: 'Cheick'
+  },
+  {
+    id: 3,
+    name: 'Oumar'
+  }
+  
+] 
+Entrepot = [
+  {
+    id: 1,
+    name: 'Coyah'
+  },
+  {
+    id: 2,
+    name: 'Boké'
+  },
+  {
+    id: 3,
+    name: 'Gbessia'
+  }
+] 
+onSubmit (): void {
+  if (this.AjustementStock.valid) {
+    // console.log('Formulaire : ', this.ajustementForm.value)
+    this.service.create('approvisionnement', 'add', this.AjustementStock.value).subscribe({
+      next: (response) => {
+        this.snackBar.open("Approvisionnement enregistre avec succès !", "Okay", {
+          duration: 3000,
+          horizontalPosition: "right",
+          verticalPosition: "top",
+          panelClass: ['bg-success', 'text-white']
 
+        })
+      },
+      error: (err) => {
+        this.snackBar.open("Erreur, Veuillez reessayer!", "Okay", {
+          duration: 3000,
+          horizontalPosition: "left",
+          verticalPosition: "top",
+          panelClass: ['bg-danger', 'text-white']
+        })
+      }
+    })
+  }
+}
 
-ngOnInit(): void {
+onReset (): void {
+  this.AjustementStock.reset()
+
 }
 
 
@@ -40,3 +132,4 @@ saveDataAjustementStock() {
   }
 }
 }
+
